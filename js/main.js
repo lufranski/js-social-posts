7d33536d6242ad7660d3f0258c095bc174f7f1da
lufranski <lufranski@gmail.com>
2022-11-23 15:18:59 +0100

    Milestone 2

---
+++ js/main.js
@@ -0,0 +1,98 @@
+const posts = [
+    {
+        "id": 1,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Phil Mangione",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 80,
+        "created": "2021-06-25"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=112",
+        "author": {
+            "name": "Sofia Perlari",
+            "image": "https://unsplash.it/300/300?image=10"
+        },
+        "likes": 120,
+        "created": "2021-09-03"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=234",
+        "author": {
+            "name": "Chiara Passaro",
+            "image": "https://unsplash.it/300/300?image=20"
+        },
+        "likes": 78,
+        "created": "2021-05-15"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=24",
+        "author": {
+            "name": "Luca Formicola",
+            "image": null
+        },
+        "likes": 56,
+        "created": "2021-04-03"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/400?image=534",
+        "author": {
+            "name": "Alessandro Sainato",
+            "image": "https://unsplash.it/300/300?image=29"
+        },
+        "likes": 95,
+        "created": "2021-03-05"
+    }
+];
+
+// Collegare il container principale al file js
+const wrapper = document.getElementById('container');
+
+// Stampare dinamicamente gli elementi dell'array nell'html
+posts.forEach(post => {
+
+    wrapper.innerHTML += 
+    `
+        <div class="post">
+            <div class="post-header">
+                <div class="user-pic">
+                    <img src=${post.author.image}>
+                </div>            
+                <div class="post-user">
+                    <div class="user-name">
+                        ${post.author.name}
+                    </div>
+                    <div class="date">    
+                        <em>${post.created}</em>
+                    </div>    
+                </div>
+            </div>
+            <div class="post-text">
+                ${post.content}
+            </div>
+            <div class="post-image">
+                <img src="${post.media}">
+            </div>
+            <div class="post-footer">    
+                <div class="like">
+                    <i class="fa-regular fa-heart"></i>
+                    Like
+                </div>    
+                Piace a ${post.likes} persone
+            </div>            
+        </div>
+    `;
+
+    
+});
